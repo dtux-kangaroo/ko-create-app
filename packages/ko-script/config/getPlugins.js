94b80781f1ad48bec95b723360dd24899ca637b9
xbrave <sunzy1993@hotmail.com>
2020-12-16 15:57:26 +0800

    add changelog update modules remove deprecated funcs and modules

--- packages/ko-script/config/getPlugins.js
+++ packages/ko-script/config/getPlugins.js
@@ -8,54 +8,51 @@
  * @LastEditTime: 2019-09-04 11:51:00
  */
 
-
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 const SimpleProgressPlugin = require('webpack-simple-progress-plugin');
 const getHtmlPlugins = require('./getHtmlPlugins');
-const getDllPlugins=require('./getDllPlugins');
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const getDllPlugins = require('./getDllPlugins');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const paths = require('./defaultPaths');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const getTopBanner=require ('./getTopBanner');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const getTopBanner = require('./getTopBanner');
 const webpack = require('webpack');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-const getRulesHappy=require('./getRulesHappy');
+const getRulesHappy = require('./getRulesHappy');
 let cleanPath = ['dist'];
 let cleanOpt = {
-    root:paths.appDirectory,
-    verbose:  false,
-    dry:      false
-  }
-module.exports = (entry) => {
+  root: paths.appDirectory,
+  verbose: false,
+  dry: false,
+};
+module.exports = (entry, isDllDisabled) => {
   const plugins = [
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename:'css/[name].[hash:6].css',
-      chunkFilename:'css/[id].[hash:6].css'
+      filename: 'css/[name].[hash:6].css',
+      chunkFilename: 'css/[id].[hash:6].css',
     }),
     new FilterWarningsPlugin({
       exclude: /Conflicting order between:/,
     }),
-    new webpack.BannerPlugin(
-      getTopBanner()
-    ),
+    new webpack.BannerPlugin(getTopBanner()),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new SimpleProgressPlugin(),
     new CaseSensitivePathsPlugin(),
-    new HardSourceWebpackPlugin(),
     new webpack.ProvidePlugin({
       _: 'lodash',
-  }),
+    }),
   ];
-  if(process.env.NODE_ENV=='production'){
-    plugins.push(new CleanWebpackPlugin(cleanPath,cleanOpt));
+  if (process.env.NODE_ENV == 'production') {
+    plugins.push(new CleanWebpackPlugin(cleanPath, cleanOpt));
   }
-  //引入 dll 文件
-  Array.prototype.push.apply(plugins, getDllPlugins());
+  if (!isDllDisabled) {
+    //引入 dll 文件
+    Array.prototype.push.apply(plugins, getDllPlugins());
+  }
   // 增加 html 输出，支持多页面应用
-  Array.prototype.push.apply(plugins, getHtmlPlugins(entry));
+  Array.prototype.push.apply(plugins, getHtmlPlugins(entry, isDllDisabled));
   //加载happypackplugin
   Array.prototype.push.apply(plugins, getRulesHappy());
   return plugins;
--- packages/ko-script/config/webpackBase.js
+++ packages/ko-script/config/webpackBase.js
@@ -7,18 +7,17 @@
  * @LastEditors  : Charles
  * @LastEditTime : 2020-01-08 16:46:39
  */
-const { differenceWith } = require('lodash');
-const webpackMerge = require('webpack-merge');
+const { mergeWithCustomize, unique } = require('webpack-merge');
 const getRules = require('./getRules');
 const getPlugins = require('./getPlugins');
 const processEntry = require('./processEntry');
-const getEntry=require ('./getEntry')
+const getEntry = require('./getEntry');
 const paths = require('./defaultPaths');
-const {isAbsolute}=require('../util/fileService');
+const { isAbsolute } = require('../util/fileService');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const TS_LOADER = require.resolve('ts-loader');
-const {createHappyPlugin}=require("../util/createHappyPlugin")
+const { createHappyPlugin } = require('../util/createHappyPlugin');
 const HAPPY_PACK = require.resolve('happypack/loader');
 
 /**
@@ -26,21 +25,12 @@ const HAPPY_PACK = require.resolve('happypack/loader');
  * @param  {array} uniques plugin 名单，在这名单内的插件会过滤掉，不会出现两份，以用户的配置为准。
  * @return {array}
  */
-const pluginsUnique = (uniques) => {
-  const getter = (plugin) => plugin.constructor && plugin.constructor.name;
-  return (a, b, k) => {
-    if (k === 'plugins') {
-      return [
-        ...differenceWith(a, b, (item, item2) => {
-          return (
-            uniques.indexOf(getter(item)) >= 0 && getter(item) === getter(item2)
-          );
-        }),
-        ...b,
-      ];
-    }
-  };
-};
+const pluginsUnique = pluginsName =>
+  unique(
+    'plugins',
+    pluginsName,
+    plugin => plugin.constructor && plugin.constructor.name
+  );
 /**
  * @description: webpack基本配置
  * @param1: param
@@ -49,80 +39,93 @@ const pluginsUnique = (uniques) => {
  * @Author: Charles
  * @Date: 2018-12-26 11:24:53
  */
-const ENV_PROD = "production";
-const ENV_DEV = "development";
+const ENV_PROD = 'production';
+const ENV_DEV = 'development';
 
 module.exports = function getWebpackBase(program) {
-  const result=getEntry(program);
-const tsRule=[
-  {
-    test: /\.(ts|tsx)$/,
-    exclude: /node_modules/,
-    loader: HAPPY_PACK,
-    options: {
-        id: "happy-babel-ts"
-    }
-}];
-  const tsPlugin=[  
-    new ForkTsCheckerWebpackPlugin({
-    async: false,
-    watch: paths.appSrc,
-    tsconfig: paths.appTsConfig
-   }),
-   createHappyPlugin('happy-babel-ts', [{
-    loader: TS_LOADER,
-    options:{
-      transpileOnly: true,
-      happyPackMode:true
-    }}])
+  const isDllDisabled = program.disableDll;
+  const result = getEntry(program);
+  const tsRule = [
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      loader: HAPPY_PACK,
+      options: {
+        id: 'happy-babel-ts',
+      },
+    },
   ];
-  
+  const tsPlugin = [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      typescript: {
+        configFile: paths.appTsConfig,
+      },
+    }),
+    //TODO: replace happy-loader instead of thread-loader: https://github.com/webpack-contrib/thread-loader
+    createHappyPlugin('happy-babel-ts', [
+      {
+        loader: TS_LOADER,
+        options: {
+          transpileOnly: true,
+          happyPackMode: true,
+        },
+      },
+    ]),
+  ];
 
   const webpackConfig = {
     mode: process.env.NODE_ENV === ENV_DEV ? ENV_DEV : ENV_PROD,
     context: paths.appDirectory,
-    output: Object.assign(
-      {
-        path: paths.appDist,
-        filename: 'js/[name].[hash:6].js',
-        publicPath:'/'
-      }
-    ),
+    output: {
+      path: paths.appDist,
+      filename: 'js/[name].[contenthash:6].js',
+      publicPath: '/',
+    },
     resolve: {
       modules: [paths.appModules, 'node_modules'],
-      extensions: ['.js', '.jsx', '.scss', '.css', '.less','.json','.html','.vue','.ts','.tsx',],
-      alias:{
-        'vue$':'vue/dist/vue.esm.js'
+      extensions: [
+        '.js',
+        '.jsx',
+        '.scss',
+        '.css',
+        '.less',
+        '.json',
+        '.html',
+        '.vue',
+        '.ts',
+        '.tsx',
+      ],
+      alias: {
+        vue$: 'vue/dist/vue.esm.js',
       },
-      plugins:program.ts?
-      [
-        new TsconfigPathsPlugin({ 
-          configFile: paths.appTsConfig,
-          extensions: ['.ts', '.tsx', '.js', '.jsx']
-         }),
-      ]:[]
+      plugins: program.ts
+        ? [
+            new TsconfigPathsPlugin({
+              configFile: paths.appTsConfig,
+              extensions: ['.ts', '.tsx', '.js', '.jsx'],
+            }),
+          ]
+        : [],
     },
     module: {
-      rules: program.ts?getRules().concat(tsRule):getRules(),
+      rules: program.ts ? getRules().concat(tsRule) : getRules(),
     },
-    performance: { //打包性能配置
+    performance: {
+      //打包性能配置
       hints: false, // 关闭性能提示
     },
-    plugins: program.ts? getPlugins(result.entry).concat(tsPlugin):getPlugins(result.entry),
+    plugins: program.ts
+      ? getPlugins(result.entry, isDllDisabled).concat(tsPlugin)
+      : getPlugins(result.entry, isDllDisabled),
     optimization: {},
-    node: {
-      dgram: 'empty',
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      child_process: 'empty'
-    },
+    node: false
   };
-   
-  const finalWebpackConfig = webpackMerge({
-    customizeArray: pluginsUnique(['HtmlWebpackPlugin'])
+
+  const finalWebpackConfig = mergeWithCustomize({
+    customizeArray: pluginsUnique(['HtmlWebpackPlugin']),
   })(webpackConfig, result.webpack);
-  finalWebpackConfig.output.path=isAbsolute(finalWebpackConfig.output.path);
-  finalWebpackConfig.entry=processEntry(finalWebpackConfig.entry);
+  finalWebpackConfig.output.path = isAbsolute(finalWebpackConfig.output.path);
+  finalWebpackConfig.entry = processEntry(finalWebpackConfig.entry);
   return finalWebpackConfig;
 };
